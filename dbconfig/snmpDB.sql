67425a2c4f1a5a87ee994a2d44450c530f6e8d98
DanG456 <80288370+DanG456@users.noreply.github.com>
2022-05-23 19:15:29 -0500

    Update snmpDB.sql

--- dbconfig/snmpDB.sql
+++ dbconfig/snmpDB.sql
@@ -3,7 +3,7 @@ CREATE DATABASE SNMPDATA;
 USE SNMPDATA;
 
 CREATE TABLE Datos_SNMP(
-    idSegmentoDatos int NOT NULL,
+    idSegmentoDatos int NOT NULL AUTO_INCREMENT,
     IPAgente VARCHAR(16),
     UserNameAgente VARCHAR(100),
     PassA VARCHAR(100),
@@ -13,7 +13,7 @@ CREATE TABLE Datos_SNMP(
 );
 
 CREATE TABLE Datos_Dispo(
-    idSolicitud int NOT NULL
+    idSolicitud int NOT NULL AUTO_INCREMENT,
     NombreDispo VARCHAR (300),
     OIDConsultado VARCHAR(30),
     Memoria int,
